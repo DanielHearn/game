4ac1b2aea43b2f31fe543127de750d4b87717551
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-04-06 23:56:28 +0100

    Cleanup

--- server.js
+++ server.js
@@ -1,34 +1,21 @@
 "use strict";
-// Optional. You will see this name in eg. 'ps' or 'top' command
-process.title = 'node-chat';
-// Port where we'll run the websocket server
-var webSocketsServerPort = 1337;
-// websocket and http servers
-var webSocketServer = require('websocket').server;
-var http = require('http');
-var history = [ ];
-var clients = [ ];
+process.title = 'node-chat'
+const webSocketsServerPort = 1337
+const webSocketServer = require('websocket').server
+const http = require('http')
+let history = [ ]
+let clients = [ ]
 
-var server = http.createServer(function(request, response) {
-  // Not important for us. We're writing WebSocket server,
-  // not HTTP server
-});
+const server = http.createServer(function(request, response) {})
 server.listen(webSocketsServerPort, function() {
   console.log((new Date()) + " Server is listening on port "
       + webSocketsServerPort);
 });
-/**
- * WebSocket server
- */
-var wsServer = new webSocketServer({
-  // WebSocket server is tied to a HTTP server. WebSocket
-  // request is just an enhanced HTTP request. For more info 
-  // http://tools.ietf.org/html/rfc6455#page-6
+
+let wsServer = new webSocketServer({
   httpServer: server
 });
 
-// This callback function is called every time someone
-// tries to connect to the WebSocket server
 wsServer.on('request', function(request) {
   console.log(`${new Date()} Connection from origin ${request.origin}`)
 
@@ -40,19 +27,18 @@ wsServer.on('request', function(request) {
     if (message.type === 'utf8') {
       const msg = message.utf8Data
       console.log(`Received Message: ${msg}`)
-      for (let client of clients) {
-        client.sendUTF(`A user sent: ${msg}`)
+      for (const client of clients) {
+        client.connection.sendUTF(`${client.name}: ${msg}`)
       }
       connection.sendUTF('Message received')
     }
-  });
-  // user disconnected
+  })
+
   connection.on('close', function(connection) {
     clients = clients.filter((client) => client.remoteAddress !== connection.remoteAddress)
     console.log(`Clients: ${clients.length}`)
-  });
-});
-
+  })
+})
 
 function createUser(connection) {
   return {
