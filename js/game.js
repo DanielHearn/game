e1b381001e7e430333c4866fe96d48651dd02069
Daniel Hearn <DanielHearn@users.noreply.github.com>
2020-04-07 21:28:43 +0100

    Add basic game loop

--- js/game.js
+++ js/game.js
@@ -1,15 +1,14 @@
 
 window.WebSocket = window.WebSocket || window.MozWebSocket
 
-// const serverIP = '86.151.188.17'
-const serverIP = '192.168.0.27';
+const serverIP = '86.151.188.17'
+//const serverIP = '192.168.0.27';
 
 const serverPort = '1337'
 let connection = null
 let canvas
 let ctx
 let connected = false
-
 
 const playerColour = "#FF0000"
 const playerSize = 32
@@ -20,6 +19,9 @@ const playerSpeed = 2.0;
 var playerId = null;
 var positionX = 0;
 var positionY = 0;
+let activeKeys = {}
+const gameSpeed = 60;
+
 window.onload = (event) => {
   init()
 }
@@ -29,34 +31,46 @@ function send(msg) {
   console.log(`Sent message: ${msg}`)
 }
 
+function iterate() {
+  window.requestAnimationFrame(interval)
+}
+
+function interval() {
+  move()
+  render()
+  setTimeout(() => { iterate() }, 1000/gameSpeed)
+}
+
+function render() {
+  ctx.fillStyle = '#000';
+  ctx.fillRect(0, 0, 500, 500);
+
+  for (const client of clientPlayers) {
+    const x = client.x
+    const y = client.y
+    drawPlayer(x, y)
+  }
+}
+
 function init() {
   console.log('Initialisation')
   connection = new WebSocket(`ws://${serverIP}:${serverPort}`)
 
   document.addEventListener('keydown', event => {
     const key = event.key.toLowerCase();
-    switch(key) {
-      case 'a':
-        positionX -= playerSpeed;
-        break;
-      case 'd':
-        positionX += playerSpeed;
-        break;
-      case 'w':
-        positionY -= playerSpeed;
-        break;
-      case 's':
-        positionY += playerSpeed;
-        
-        break;
-    }
-    move();
+    activeKeys[key] = true
+  });
+
+  document.addEventListener('keyup', event => {
+    const key = event.key.toLowerCase();
+    delete activeKeys[key]
   });
 
   connection.onopen = function () {
     console.log('Connected')
     connected = true
     showConnectionStatus()
+    iterate()
   }
   
   connection.onerror = function (error) {
@@ -64,23 +78,13 @@ function init() {
   }
   
   connection.onmessage = function (message) {
-    
     const client = JSON.parse(message.data)
     updateClientPlayer(client);
-    console.log(client);
-    if (playerId == null) {
+    if (playerId === null) {
       playerId = client.id;
     }
-    ctx.fillStyle = '#000';
-    ctx.fillRect(0, 0, 500, 500);
-    for (const client of clientPlayers) {
-
-      const x = client.x
-      const y = client.y
-      console.log(client);
-      drawPlayer(x, y)
-    }
   };
+
   initCanvas()
   sendButton.addEventListener('click', () => {
     send('Random text')
@@ -88,7 +92,48 @@ function init() {
 }
 
 function move(){
-  send(JSON.stringify({id:playerId, x:positionX, y:positionY}))
+  let newX = positionX
+  let newY = positionY
+  let alreadyMoving = false
+  if (activeKeys['a']) {
+    if(alreadyMoving) {
+      newX -= playerSpeed/2
+    } else {
+      newX -= playerSpeed
+    }
+
+    alreadyMoving = true
+  }
+  if (activeKeys['w']) {
+    if(alreadyMoving) {
+      newY -= playerSpeed/2
+    } else {
+      newY -= playerSpeed
+    }
+    alreadyMoving = true
+  }
+  if (activeKeys['d']) {
+    if(alreadyMoving) {
+      newX += playerSpeed/2
+    } else {
+      newX += playerSpeed
+    }
+    alreadyMoving = true
+  }
+  if (activeKeys['s']) {
+    if(alreadyMoving) {
+      newY += playerSpeed/2
+    } else {
+      newY += playerSpeed
+    }
+    alreadyMoving = true
+  }
+  if (positionX !== newX || positionY !== newY) {
+    positionX = newX
+    positionY = newY
+  
+    send(JSON.stringify({id:playerId, x:positionX, y:positionY}))
+  }
 }
 
 /*
@@ -96,17 +141,17 @@ function move(){
   if not, then just add it, and keep track. Otherwise, update that players' position.
 */
 function updateClientPlayer(data) {
-  var clientPlayerExists = false;
-  var clientPlayerID = data.id;
+  let clientPlayerExists = false;
+  const clientPlayerID = data.id;
   for (const index in clientPlayers) {
-    var client = clientPlayers[index];
+    const client = clientPlayers[index];
 
-    if (client.id == clientPlayerID) {
+    if (client.id === clientPlayerID) {
       clientPlayerExists = true;
-      var clientPlayerX = data.x;
-      var clientPlayerY = data.y;
-      clientPlayers[index].x = clientPlayerX;
-      clientPlayers[index].y = clientPlayerY;
+      const clientPlayerX = data.x;
+      const clientPlayerY = data.y;
+      client.x = clientPlayerX;
+      client.y = clientPlayerY;
       break;
     }
   }
@@ -126,11 +171,9 @@ function initCanvas() {
   canvas = document.querySelector("#drawable");
   ctx = canvas.getContext("2d");
   ctx.fillRect(0, 0, 500, 500)
-
 }
 
 function drawPlayer(x, y) {
   ctx.fillStyle = playerColour
-  console.log("test")
   ctx.fillRect(x, y, playerSize, playerSize)
 }
